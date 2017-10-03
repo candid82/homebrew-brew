3792ada76a16f31e7488bba511357b2bfa5766b2
Roman Bataev <roman.bataev@gmail.com>
2017-10-02 22:10:20 -0700

    joker-v0.8.5

--- joker.rb
+++ joker.rb
@@ -1,9 +1,9 @@
 class Joker < Formula
   desc "Small Clojure interpreter and linter"
   homepage "https://github.com/candid82/joker"
-  url "https://github.com/candid82/joker/releases/download/v0.8.4/joker-0.8.4-mac-amd64.zip"
-  sha256 "ed8db220c37016033c22831497efea22d2c57e30272ab51a7821376455296760"
-  version "0.8.4"
+  url "https://github.com/candid82/joker/releases/download/v0.8.5/joker-0.8.5-mac-amd64.zip"
+  sha256 "318543e6a812e62d340a093c1d2afc8fd3736985cdca7a5964f2a439469981ec"
+  version "0.8.5"
 
   bottle :unneeded
 
