413c7160afbd3420b058ea7e07f11f6b59110761
Roman Bataev <rbataev@walmartlabs.com>
2020-04-07 21:13:33 -0700

    joker-v0.15.3

--- joker.rb
+++ joker.rb
@@ -1,14 +1,14 @@
 class Joker < Formula
   desc "Small Clojure interpreter and linter"
   homepage "https://github.com/candid82/joker"
-  version "0.15.2"
+  version "0.15.3"
 
   if OS.linux?
-    url "https://github.com/candid82/joker/releases/download/v0.15.2/joker-0.15.2-linux-amd64.zip"
-    sha256 "bfc452efbc61193a08b60ffd5c239a32edc4df857f42266374714f3b883de98a"
+    url "https://github.com/candid82/joker/releases/download/v0.15.3/joker-0.15.3-linux-amd64.zip"
+    sha256 "2cd9764128c09540101b998a2ddd8279db7e6db6986a4e1d2d30f8809fff2e28"
   else
-    url "https://github.com/candid82/joker/releases/download/v0.15.2/joker-0.15.2-mac-amd64.zip"
-    sha256 "9dc8b18de7dfea4d49db36a02e546460adfc3320a2d034f404b3f207f9422317"
+    url "https://github.com/candid82/joker/releases/download/v0.15.3/joker-0.15.3-mac-amd64.zip"
+    sha256 "da167d7d8767a6a69748ed3eb5e481312f3ed0751f997b566caf6493c533d632"
   end
 
   bottle :unneeded
