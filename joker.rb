cbd76be2bd9ae6a4f9f7a44475490f3c199c6e42
Roman Bataev <roman@bataev.me>
2022-09-03 22:04:56 -0400

    Update download urls.

--- joker.rb
+++ joker.rb
@@ -4,10 +4,10 @@ class Joker < Formula
   version "1.0.1"
 
   if OS.linux?
-    url "https://github.com/candid82/joker/releases/download/v1.0.1/joker-1.0.1-linux-amd64.zip"
+    url "https://github.com/candid82/joker/releases/download/v1.0.1/joker-linux-amd64.zip"
     sha256 "b70029dac7651e5ef62a7de2f37169b4ef488ab0cb93786a88d37d09e3f4f7f9"
   else
-    url "https://github.com/candid82/joker/releases/download/v1.0.1/joker-1.0.1-mac-amd64.zip"
+    url "https://github.com/candid82/joker/releases/download/v1.0.1/joker-mac-amd64.zip"
     sha256 "49f8b6f3323e925829dd3075fb7fdf38cb44aa7c78245b690bf9a3b64a9a134b"
   end
 
