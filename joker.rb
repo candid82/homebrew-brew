e26dd25b9398d88918b7ddca71dc846036e5d26b
Roman Bataev <roman@bataev.me>
2023-12-21 11:25:07 -0500

    joker-v1.3.3

--- joker.rb
+++ joker.rb
@@ -1,14 +1,14 @@
 class Joker < Formula
   desc "Small Clojure interpreter and linter"
   homepage "https://github.com/candid82/joker"
-  version "1.3.2"
+  version "1.3.3"
 
   if OS.linux?
-    url "https://github.com/candid82/joker/releases/download/v1.3.2/joker-linux-amd64.zip"
-    sha256 "029057f4d103e68d59b2c050fc8968d7da05a73575106b5de0dcbad4a27776b0"
+    url "https://github.com/candid82/joker/releases/download/v1.3.3/joker-linux-amd64.zip"
+    sha256 "01326bbb6f178d22b0cc30b043064c4da97c7965acba3159fac6582d19f44a5f"
   else
-    url "https://github.com/candid82/joker/releases/download/v1.3.2/joker-mac-amd64.zip"
-    sha256 "0a19a3090d524d82cb3e1e524de6243c296b01d120bbf903d1fbedbc0ee6d295"
+    url "https://github.com/candid82/joker/releases/download/v1.3.3/joker-mac-amd64.zip"
+    sha256 "066d4adbffedbc1dd4f0a3a9ecb3d8f6f02c86e54b9f360fc2a503909c2d279c"
   end
 
   def install
