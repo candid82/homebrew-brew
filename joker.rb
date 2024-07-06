30252d2d0e06325a934881723fe92a8c5d2e4d55
Roman Bataev <roman@bataev.me>
2024-07-06 19:48:23 -0400

    joker-v1.4.0

--- joker.rb
+++ joker.rb
@@ -1,14 +1,14 @@
 class Joker < Formula
   desc "Small Clojure interpreter and linter"
   homepage "https://github.com/candid82/joker"
-  version "1.3.5"
+  version "1.4.0"
 
   if OS.linux?
-    url "https://github.com/candid82/joker/releases/download/v1.3.5/joker-linux-amd64.zip"
-    sha256 "ce1b0325610109f2819c0712b66bb8a4278b42f1224d92bd6a55435e8533745b"
+    url "https://github.com/candid82/joker/releases/download/v1.4.0/joker-linux-amd64.zip"
+    sha256 "c38458e7933e23b41c0d978a1fe3c20de5c7c1970a28c4ec5e0d43440e80b92b"
   else
-    url "https://github.com/candid82/joker/releases/download/v1.3.5/joker-mac-amd64.zip"
-    sha256 "63a1de9916173ef2e1f184e0269572252d4960e5cc2a443ee0fe7cb0ede65fca"
+    url "https://github.com/candid82/joker/releases/download/v1.4.0/joker-mac-amd64.zip"
+    sha256 "735c52387f08eab6aaa87c690c8c2f3a836ce069fb5c2d63081920350d636f63"
   end
 
   def install
